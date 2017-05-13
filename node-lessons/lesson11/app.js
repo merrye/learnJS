a811440597b7f7a647f63ab13cbffe819de51d82
Merrye <ye598921631@163.com>
2017-05-13 20:03:19 +0800

    lesson11:作用域与闭包

---
+++ node-lessons/lesson11/app.js
@@ -0,0 +1,14 @@
+// var parent = function(){
+//     var name = 'parent_name',
+//         age = 13,
+//         child = function(){
+//             var name = 'child_name',
+//                 childAge = 0.3;
+//             console.log(name,age,childAge);
+//         };
+//         child();
+           // will throw Error
+           // ReferenceError: childAge is not defined    
+//         console.log(name, age , childAge);
+// };
+// parent();
