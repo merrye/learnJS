58baeff08525095406ee3df4bad12d40c23763f1
Merrye <ye598921631@163.com>
2017-07-31 11:21:00 +0800

    mvc

--- review/NodeJs study/03-koa/app.js
+++ review/NodeJs study/03-koa/app.js
@@ -1,15 +1,36 @@
 const Koa = require('koa'),
     bodyParser = require('koa-bodyparser'),
+    isProduction = process.env.NODE_ENV === 'production',
     controller = require('./controller'),
+    templating = require('./templating'),
     app = new Koa();
 
+// 记录URL以及页面执行的时间
 app.use(async (ctx , next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    let start = new Date().getTime(),
+        exeTime;
     await next();
+    execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time' , `${execTime}`);
 });
 
+// 处理静态文件
+if(!isProduction){
+    let staticFiles = require('./static-files.js');
+    app.use(staticFiles('/static/' , __dirname + '/static'));
+};
+
+// 处理POST请求
 app.use(bodyParser());
 
+// templatimg
+app.use(templating('views' , {
+    noCache: !isProduction,
+    wathc: !isProduction,
+}));
+
+// router
 app.use(controller());
 
 app.listen(3000);
--- review/NodeJs study/03-koa/controllers/index.js
+++ review/NodeJs study/03-koa/controllers/index.js
@@ -1,23 +1,38 @@
 const fn_index = async (ctx , next) => {
-        ctx.response.body = `<h1>Index</h1>
-            <form action='/signin' method="post">
-                <p>Name: <input name="name" value="koa"></p>
-                <p>Password: <input name="Password" type="password"></p>
-                <p><input type="submit" value="submit"></p>
-            <form>
-        `;
+        ctx.render('index.html' , {
+            title: 'Welcome'
+        });
+        // ctx.response.body = `<h1>Index</h1>
+        //     <form action='/signin' method="post">
+        //         <p>Name: <input name="name" value="koa"></p>
+        //         <p>Password: <input name="Password" type="password"></p>
+        //         <p><input type="submit" value="submit"></p>
+        //     <form>
+        // `;
     },
     fn_signin = async (ctx , next) => {
-        const name = ctx.request.body.name || '',
-            password = ctx.request.body.Password || '';
-        console.log(`signin with name: ${name} , password: ${password}`);
-        if(name === 'koa' && password === '12345'){
-            ctx.response.body = `<h1>Welcome , ${name}</h1>`;
+        const email = ctx.request.body.email || '',
+            password = ctx.request.body.password || '';
+        if(email === 'admin@example.com' && password === '123456'){
+            ctx.render('signin-ok.html' , {
+                title: 'Sign In OK',
+                name: "Mr Node"
+            });
         }else{
-            ctx.response.body = `<h1>Login faileds!</h1>
-                <p><a href="/">Try again</a></p>
-            `;
+            ctx.render('signin-failed.html' , {
+                title: 'Sign In Failed',
+            })
         };
+        // const name = ctx.request.body.name || '',
+        //     password = ctx.request.body.Password || '';
+        // console.log(`signin with name: ${name} , password: ${password}`);
+        // if(name === 'koa' && password === '12345'){
+        //     ctx.response.body = `<h1>Welcome , ${name}</h1>`;
+        // }else{
+        //     ctx.response.body = `<h1>Login faileds!</h1>
+        //         <p><a href="/">Try again</a></p>
+        //     `;
+        // };
     };
 
 module.exports = {
