e0c0a77661a3ea23eec8ab0772f7311adda40b9a
Merrye <ye598921631@163.com>
2017-08-02 15:34:41 +0800

    rest开发

--- review/NodeJs study/07-rest/app.js
+++ review/NodeJs study/07-rest/app.js
@@ -1,9 +1,26 @@
 const Koa = require('koa'),
-    bodyParser = require('koa-bodyparser'),
+    rest = require('./rest'),
     controller = require('./controller'),
+    templating = require('./templating'),
+    bodyParser = require('koa-bodyparser'),
+    staticFiles = require('./static-files'),
     app = new Koa();
 
+app.use(async (ctx , next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+app.use(staticFiles('/static' , __dirname + '/static/'));
+
 app.use(bodyParser());
+
+app.use(templating('views' , {
+    noCache: true,
+    watch: true
+}));
+
+app.use(rest.restify());
 
 app.use(controller());
 
--- review/NodeJs study/07-rest/controllers/api.js
+++ review/NodeJs study/07-rest/controllers/api.js
@@ -1,25 +1,33 @@
-const products = [{
-        name: 'iPhone',
-        price: 6999,
-    },{
-        name: 'Kindle',
-        price: 999
-    }];
+const products = require('../products'),
+    APIError = require('../rest').APIError;
 
 module.exports = {
     "GET /api/products": async (ctx , next) => {
-        ctx.response.type = 'application/json';
-        ctx.response.body = {
-            products
-        }
+        ctx.rest({
+            products: products.getProducts()
+        });
+        // ctx.response.type = 'application/json';
+        // ctx.response.body = {
+        //     products
+        // }
     },
     "POST /api/products": async(ctx , next) => {
-        const p = {
-            name: ctx.request.body.name,
-            price: ctx.request.body.price
+        const p = products.createProduct(ctx.request.body.name , ctx.request.body.manufacturer , parseFloat(ctx.request.body.price));
+        ctx.rest(p);
+        // const p = {
+        //     name: ctx.request.body.name,
+        //     price: ctx.request.body.price
+        // };
+        // products.push(p);
+        // ctx.response.type = 'application/json';
+        // ctx.response.body = p;
+    },
+    "DELETE /api/products/:id": async(ctx , next) => {
+        let p = procucts.deleteProduct(ctx.params.id);
+        if(p){
+            ctx.rest(p);
+        }else{
+            throw new APIError('product:not_found' , 'product not found by id.');
         };
-        products.push(p);
-        ctx.response.type = 'application/json';
-        ctx.response.body = p;
     }
 };
