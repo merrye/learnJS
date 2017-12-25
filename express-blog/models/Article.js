e9fae9ac709923c2e492edfe7064095d801f5a24
Merry <ye598921631@163.com>
2017-12-25 21:57:02 +0800

    modify the article table structure

--- express-blog/models/Article.js
+++ express-blog/models/Article.js
@@ -2,7 +2,6 @@ const db = require("../module/db");
 
 module.exports = db.defineModel("articles" , {
     content: db.TEXT,
-    path: db.STRING(255),
     href: db.STRING(255),
     title: db.STRING(255),
     description: db.STRING(255),
