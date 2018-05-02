13ea3874cd0f7eed52e9dfd762a831ec20bac275
Merry <merrye.com@gmail.com>
2018-05-02 20:40:32 +0800

    delete all .vscode and node_modules folder

--- JS/nodeJS/01/module/02.js
+++
@@ -1,3 +0,0 @@
-// JavaScript source code
-const b = require('./01').a;
-console.log(b);
--- JS/nodeJS/03/test.js
+++
@@ -1,8 +0,0 @@
-let arr = [{name: 'Merry',age: 14},{name: 'Merrye',age: 10},{name: 'Merrypc',age: 12}];
-let newArr = [1,3,4,6];
-let nArr = [1,2,3];
-for(let [key,value] of arr.entries()){
-    for(let [key,val] of Object.entries(value)){
-        console.log(`${key} : ${val} \n aa`);
-    };
-};
